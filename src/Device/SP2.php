4d91d7e3fea10d0d5e17cd81371e3c1af104e1f0
Sam Van der Borght <samvanderborght@gmail.com>
2017-11-28 21:45:23 +0100

    Update typehints

--- src/Device/SP2.php
+++ src/Device/SP2.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Broadlink\Device;
 
@@ -8,7 +8,27 @@ use Broadlink\Util\Encryption;
 
 class SP2 extends AbstractDevice
 {
-    public function Set_Power($state){
+
+    /**
+     * @return bool
+     */
+    public function getPower()
+    {
+        return $this->Check_Power();
+    }
+
+    /**
+     * @param boolean $state
+     * @return void
+     */
+    public function setPower($state)
+    {
+        $this->Set_Power($state);
+    }
+
+
+    public function Set_Power($state)
+    {
 
         $packet = ByteUtil::bytearray(16);
         $packet[0] = 0x02;
@@ -17,17 +37,18 @@ class SP2 extends AbstractDevice
         $this->getClient()->send(0x6a, $packet);
     }
 
-    public function Check_Power(){
+    public function Check_Power()
+    {
 
         $packet = ByteUtil::bytearray(16);
         $packet[0] = 0x01;
 
         $response = $this->getClient()->send(0x6a, $packet);
         $err = hexdec(sprintf("%x%x", $response[0x23], $response[0x22]));
-        if($err == 0){
+        if ($err == 0) {
             $enc_payload = array_slice($response, 0x38);
 
-            if(count($enc_payload) > 0){
+            if (count($enc_payload) > 0) {
 
                 $payload = ByteUtil::byte2array(Encryption::decrypt($this->getClient()->getKey(), ByteUtil::byte($enc_payload), $this->getClient()->getIv()));
                 return $payload[0x4] ? true : false;
